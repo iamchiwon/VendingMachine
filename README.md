c11d66e7ce66beec6c84778d7321512e8d45bcfb
iamchiwon <iamchiwon@itsdcode.com>
2018-08-27 17:01:27 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VendingMachine
-Swift 함수형 프로그래밍 예제 (자동판매기 만들기)
+
+### Swift 함수형 프로그래밍 예제 (자동판매기 만들기)
+
+![screenshot](screenshot.gif)
+
---
+++ VendingMachine/VendingMachine/ViewController.swift
@@ -0,0 +1,175 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by bearfried on 2018. 8. 27..
+//  Copyright © 2018년 bearfried. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Model
+
+    enum Product: Int {
+        case cola = 1000
+        case cider = 1100
+        case fanta = 1200
+        func name() -> String {
+            switch self {
+            case .cola: return "콜라"
+            case .cider: return "사이다"
+            case .fanta: return "환타"
+            }
+        }
+    }
+
+    enum Input {
+        case moneyInput(Int)
+        case productSelect(Product)
+        case reset
+        case none
+    }
+
+    enum Output {
+        case displayMoney(Int)
+        case productOut(Product)
+        case shortMoneyError
+        case change(Int)
+    }
+
+    struct State {
+        let money: Int
+        static func initial() -> State {
+            return State(money: 0)
+        }
+    }
+
+    // UI
+
+    @IBOutlet weak var displayMoney: UILabel!
+
+    @IBOutlet weak var productOut: UIImageView!
+
+    @IBOutlet weak var textInfo: UILabel!
+
+    @IBAction func money100(_ sender: Any) {
+        processHandler("100")
+    }
+
+    @IBAction func money500(_ sender: Any) {
+        processHandler("500")
+    }
+
+    @IBAction func money1000(_ sender: Any) {
+        processHandler("1000")
+    }
+
+    @IBAction func selectCola(_ sender: Any) {
+        processHandler("cola")
+    }
+
+    @IBAction func selectCider(_ sender: Any) {
+        processHandler("cider")
+    }
+
+    @IBAction func selectFanta(_ sender: Any) {
+        processHandler("fanta")
+    }
+
+    @IBAction func reset(_ sender: Any) {
+        processHandler("reset")
+    }
+
+    // Logic
+
+    lazy var processHandler = handleProcess(State.initial())
+
+    func handleProcess(_ state: State) -> (String) -> () {
+        var state = state //memoization
+        return { command in
+            state = self.operation(self.uiInput(command), self.uiOutput)(state)
+        }
+    }
+
+    func uiInput(_ command: String) -> () -> Input {
+        return {
+            switch command {
+            case "100": return .moneyInput(100)
+            case "500": return .moneyInput(500)
+            case "1000": return .moneyInput(1000)
+            case "cola": return .productSelect(.cola)
+            case "cider": return .productSelect(.cider)
+            case "fanta": return .productSelect(.fanta)
+            case "reset": return .reset
+            default: return .none
+            }
+        }
+    }
+
+    func uiOutput(_ output: Output) -> Void {
+        switch output {
+        case .displayMoney(let m):
+            displayMoney.text = "\(m)"
+
+        case .productOut(let p):
+            switch p {
+            case .cola:
+                productOut.image = #imageLiteral(resourceName: "cola_l")
+            case .cider:
+                productOut.image = #imageLiteral(resourceName: "cider_l")
+            case .fanta:
+                productOut.image = #imageLiteral(resourceName: "fanta_l")
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.productOut.image = nil
+            }
+
+        case .shortMoneyError:
+            textInfo.text = "잔액이 부족합니다."
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.textInfo.text = ""
+            }
+
+        case .change(let c):
+            textInfo.text = "\(c)"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.textInfo.text = ""
+            }
+        }
+    }
+
+    func operation(_ inp: @escaping () -> Input, _ out: @escaping (Output) -> Void) -> (State) -> State {
+        return { state in
+            let input = inp()
+
+            switch input {
+            case .moneyInput(let m):
+                let money = state.money + m
+                out(.displayMoney(money))
+                return State(money: money)
+
+            case .productSelect(let p):
+                if state.money < p.rawValue {
+                    out(.shortMoneyError)
+                    return state
+                }
+                out(.productOut(p))
+                let money = state.money - p.rawValue
+                out(.displayMoney(money))
+                return State(money: money)
+
+            case .reset:
+                out(.change(state.money))
+                out(.displayMoney(0))
+                return State(money: 0)
+
+            case .none:
+                return state
+            }
+        }
+    }
+
+}
+
